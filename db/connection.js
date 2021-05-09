a150a602c0d39ec7c69d9d5574a40bf0f34522d4
Andres Wellmann <wellmann39@gmail.com>
2021-05-09 12:00:14 -0600

    Prueba de host

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 // Conexion a la base MSSQL
 const db = new Sequelize('Cafeteria', process.env.SQL_NAME, process.env.SQL_PASS, {
-    host: 'localhost',
+    host: 'localhost' || process.env.PORT,
     dialect: 'mssql',
     // logging: false,
 });
